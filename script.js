c30263927220891828a87bd4be7f0778d0baf6ae
Abdsamerhusari1 <107226621+Abdsamerhusari1@users.noreply.github.com>
2023-09-01 00:01:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let sections = [
+      {btnId: "education-toggle", contentId: "education-content"},
+      {btnId: "work-toggle", contentId: "work-content"},
+      {btnId: "projects-toggle", contentId: "projects-content"}
+    ];
+    
+    // Hide all sections except the clicked one
+    function hideOtherSections(currentContentId) {
+      sections.forEach(section => {
+        let content = document.getElementById(section.contentId);
+        if(section.contentId !== currentContentId) {
+          content.style.display = 'none';
+        }
+      });
+    }
+  
+    // Toggle function
+    function toggleDropdown(btnId, contentId) {
+      let btn = document.getElementById(btnId);
+      let content = document.getElementById(contentId);
+  
+      btn.addEventListener("click", function() {
+        if (content.style.display === 'none' || content.style.display === '') {
+          hideOtherSections(contentId);
+          content.style.display = 'block';
+        } else {
+          content.style.display = 'none';
+        }
+      });
+    }
+  
+    // Initialize toggles
+    sections.forEach(section => {
+      toggleDropdown(section.btnId, section.contentId);
+    });
+  });
+  
